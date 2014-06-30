406c57c4b093b12972cfeecc6cedcbe7074dc440
tdhris <tdhristova@yahoo.com>
2014-06-30 19:18:14 +0300

    fixing highlighter

--- Week4/WorkingWithLibraries/src/QuickCodeInspector.java
+++ Week4/WorkingWithLibraries/src/QuickCodeInspector.java
@@ -1,13 +1,8 @@
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import javax.swing.JFrame;
-import javax.swing.plaf.basic.BasicComboBoxUI;
 
 import syntaxhighlight.SyntaxHighlighter;
 import syntaxhighlighter.SyntaxHighlighterParser;
@@ -27,40 +22,17 @@ public class QuickCodeInspector {
     }
     
     public static void main(String[] args) throws IOException {
-        String folder = args[0];
-        File[] files = new File(folder).listFiles();
-        
         SyntaxHighlighterParser parser = new SyntaxHighlighterParser(new BrushJava());
         SyntaxHighlighter highlighter = new SyntaxHighlighter(parser, new ThemeRDark());
+        highlighter.setContent(new File(args[0]));
         
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationByPlatform(true);
         frame.setFocusable(true);
-        frame.setSize(100, 100);
         frame.pack();
         frame.setVisible(true);
-        frame.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-            
-            @Override
-            public void keyReleased(KeyEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-            
-            @Override
-            public void keyPressed(KeyEvent e) {
-                int keyCode = e.getKeyCode();
-                int arrow_right = 39;
-                if (keyCode == arrow_right) {
-                    
-                }
-            }
-        });
+        frame.setContentPane(highlighter);
+      
     }
 }
